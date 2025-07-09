d1b10a8a0ba3d0511e0ff996e69fb3e1ab9b774a
Lide777 <tiguit78@yahoo.fr>
2025-07-09 11:32:01 +0400

    ytrezsqwxcvbjklo

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,5 +9,6 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(arg_count))
+
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
