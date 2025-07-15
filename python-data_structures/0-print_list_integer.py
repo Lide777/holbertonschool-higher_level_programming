1dbc6b49f0fcb8d6d7b57611882841021b58f877
Lide777 <tiguit78@yahoo.fr>
2025-07-15 09:52:50 +0400

    azertyuiop

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{:d}".format(i))
+def print_element_at_index(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        print("Index out of range")
+    else:
+        print("{:d}".format(my_list[idx]))
