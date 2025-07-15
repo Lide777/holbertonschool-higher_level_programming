2e3dbd2e88d7834d90e9f724668eacd0607b3026
Lide777 <tiguit78@yahoo.fr>
2025-07-15 10:06:01 +0400

    azertyuiop

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def print_element_at_index(my_list, idx):
+def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
-        print("Index out of range")
-    else:
-        print("{:d}".format(my_list[idx]))
+        return None
+    return my_list[idx]
